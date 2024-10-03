125d82515d7551078af2edb7a8d8836a115ea674
Taufik <taufik@academiaworks.id>
2024-10-03 11:51:38 +0700

    add log biasa

--- src/flogger.cr
+++ src/flogger.cr
@@ -1,4 +1,4 @@
-#membuat Log dalam bentuk teks
+# membuat Log dalam bentuk teks
 require "file_utils"
 
 module Flogger
@@ -6,21 +6,28 @@ module Flogger
 
   def self.new(level : String, message : String)
     begin
-        timestamp = Time.local.to_s("%Y-%m-%d %H:%M:%S")
-        log_entry = "#{timestamp} [#{level}] - #{message}"
+      timestamp = Time.local.to_s("%Y-%m-%d %H:%M:%S")
+      log_entry = "#{timestamp} [#{level}] - #{message}"
 
-        fileName = Time.local.to_s("%Y/%m/%Y-%m-%d.log")
-        logFilePath = "./logs/#{fileName}"
+      fileName = Time.local.to_s("%Y/%m/%Y-%m-%d.log")
+      logFilePath = "./logs/#{fileName}"
 
-        # Pastikan direktori logs ada, jika tidak, buat secara rekursif
-        FileUtils.mkdir_p(File.dirname(logFilePath))
+      # Pastikan direktori logs ada, jika tidak, buat secara rekursif
+      FileUtils.mkdir_p(File.dirname(logFilePath))
 
-        # Tulis log ke file logFilePath
-        File.open(logFilePath, "a") do |file|
-            file.puts(log_entry)
-        end
+      # Tulis log ke file logFilePath
+      File.open(logFilePath, "a") do |file|
+        file.puts(log_entry)
+      end
+
+      if level == "ERROR"
+        Log.error{ message }
+      else
+        Log.info{ message }
+      end
+
     rescue
-        puts "tidak bisa membuat log"
+      puts "tidak bisa membuat log"
     end
   end
 end
